1c13608209e9ace204b975898953a807f425a306
Vinayak-Nayyar <vinayaknayyar30@gmail.com>
2026-08-14 02:44:16 +0530

    Time: 4 ms (34.08%), Space: 9.9 MB (11.54%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char,int> freq;
+        
+        for(char c : s){
+            freq[c]++;
+        }
+        for(char q : t){
+            freq[q]--;
+        }
+        for(auto x : freq){
+            if(x.second==0){
+                return true;
+            }
+            else return false;
+        }
+        return {};
+    }
+};
