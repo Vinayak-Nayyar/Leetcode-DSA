4ee5c194a434527ea350cad867bed196476c56d6
Vinayak-Nayyar <vinayaknayyar30@gmail.com>
2026-08-15 18:39:59 +0530

    Time: 15 ms (69.23%), Space: 26.1 MB (50.43%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -3,14 +3,14 @@ public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         unordered_map<string,vector<string>> ga;
         vector<vector<string>> ans;
-        for( auto c:strs){
-            string key=c;
-            sort(key.begin(),key.end());
-            ga[key].push_back(c);
-        }
-        for (auto x : ga){
-            ans.push_back(x.second);
-        }
-        return ans;
+       for(auto x:strs){
+        string key= x;
+        sort(key.begin(),key.end());
+        ga[key].push_back(x);
+       }
+       for(auto c:ga){
+        ans.push_back(c.second);
+       }
+       return ans;
     }
 };
